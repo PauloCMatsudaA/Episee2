a086df15340c9b82d330ce52459998d58aaee47e
Paulo Cesar Matsuda Almeida <pcmatsuda2010@gmail.com>
2026-06-26 21:21:33 -0300

    hotfix: corrige import ProvedorAutenticacao -> AuthProvider em main.jsx

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { ProvedorAutenticacao } from './contexts/AuthContext';
+import { AuthProvider } from './contexts/AuthContext';
 import App from './App';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ProvedorAutenticacao>
+      <AuthProvider>
         <App />
-      </ProvedorAutenticacao>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
